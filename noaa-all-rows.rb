eed2e21e6ad0611ff1213c7063ed6bb46a1d4dab
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-29 20:40:10 -0700

    what a difference a colon makes

--- noaa-all-rows.rb
+++ noaa-all-rows.rb
@@ -1,4 +1,4 @@
-# I didn't get the same results as the author. 
+# What a difference a colon makes!
 File.open("data/wksst8110.for") do |file|
   weeks = []
 
@@ -7,8 +7,9 @@ File.open("data/wksst8110.for") do |file|
       date: file.read(10).strip,
       temps: {}
     }
+    #these :nino things are symbol keys made on the fly.
     [:nino12, :nino3, :nino34, :nino4].each do |region|
-      week[:temps][:region] = {
+      week[:temps][region] = {
         temp: file.read(9).to_f,
         change: file.read(4).to_f
       }
@@ -27,7 +28,19 @@ end
 #     [   0] {
 #          :date => "03JAN1990",
 #         :temps => {
-#             :region => {
+#             :nino12 => {
+#                   :temp => 23.4,
+#                 :change => -0.4
+#             },
+#              :nino3 => {
+#                   :temp => 25.1,
+#                 :change => -0.3
+#             },
+#             :nino34 => {
+#                   :temp => 26.6,
+#                 :change => 0.0
+#             },
+#              :nino4 => {
 #                   :temp => 28.6,
 #                 :change => 0.3
 #             }
@@ -36,7 +49,19 @@ end
 #     [   1] {
 #          :date => "10JAN1990",
 #         :temps => {
-#             :region => {
+#             :nino12 => {
+#                   :temp => 23.4,
+#                 :change => -0.8
+#             },
+#              :nino3 => {
+#                   :temp => 25.2,
+#                 :change => -0.3
+#             },
+#             :nino34 => {
+#                   :temp => 26.6,
+#                 :change => 0.1
+#             },
+#              :nino4 => {
 #                   :temp => 28.6,
 #                 :change => 0.3
 #             }
@@ -45,9 +70,21 @@ end
 #     [   2] {
 #          :date => "17JAN1990",
 #         :temps => {
-#             :region => {
+#             :nino12 => {
+#                   :temp => 24.2,
+#                 :change => -0.3
+#             },
+#              :nino3 => {
+#                   :temp => 25.3,
+#                 :change => -0.3
+#             },
+#             :nino34 => {
+#                   :temp => 26.5,
+#                 :change => -0.1
+#             },
+#              :nino4 => {
 #                   :temp => 28.6,
 #                 :change => 0.3
 #             }
 #         }
-#     }, #...
+#     },
